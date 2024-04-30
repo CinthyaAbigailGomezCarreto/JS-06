4869c7987222477a1cd307fa2f909e086158a942
CinthyaAbigailGomezCarreto <164213827+CinthyaAbigailGomezCarreto@users.noreply.github.com>
2024-04-30 16:46:16 -0600

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,37 +1,50 @@
+* [] Crear seccioner p/ tarjeta
+ * Img
+ * Nombre, desc, edad, lista de algo
+ * [] Insertar en la tarjeta
+ * [] Mostrar dentro de div.contyainer
+ */
+
+const CARDS_CONTAINER = document.querySelector("#card-container");
+const USER = {
+    id: 1,
+    username: "User Name",
+    desc: "sobre mi",
+    age: 26,
+    fav_books: { // esta es una colección, es una array 
+        books: [
+            "Ensayo sobre la ceguera",
+            "Guerra y Paz",
+            "Los Miserables",
+        ]
+    }
+}
+
+// Crear tarjeta
+const card = document.createElement("div");
+const name_section = document.createElement("h3");
+const desc_section = document.createElement("p");
+const age_section = document.createElement("p");
+const book_section = document.createElement("div");
+
+// lista------------------------------------------------------------
+
+// map: es una iteracion por cada elemento, que SI regresa algo
+const bookList = USER.fav_books.books.map((e) => {
+    const item = document.createElement("ul");
+    item.textContent = e;
+    return item;
+});
 
 
-    /**
-     * crear tarjeta
-     * crear secciones p/tarjetas
-     * img
-     * nombre, descripccion, edad y la lista de algo
-     * [] insertar en la tarjeta
-     * [] mostrar dentro de div.container
-    */
+//Crear tarjeta
+// Inyectamos info
+name_section.textContent = USER.username;
+desc_section.textContent = USER.desc;
+age_section.textContent = USER.age;
+book_section.append(...bookList);
 
-    //cuando es necesario usar varibles globales es cuando se ponen en mayusculas, pero en general usar variables globales asi no es buena practica de java
+card.append(name_section, desc_section, age_section, book_section);
 
-    //dato tambien con #
-    // clase .
-   
-    
-    const users = [
-      {
-        id: 1,
-        username: "booklover123",
-        description: "Avid reader and coffee enthusiast.",
-        age: 29,
-        fav_books: {
-          books: ["To Kill a Mockingbird", "1984", "Pride and Prejudice"]
-        }
-      },
-      {
-        id: 2,
-        username: "literature_junkie",
-        description: "Poetry lover and amateur writer.",
-        age: 35,
-        fav_books: {
-          books: ["The Great Gatsby", "Invisible Man", "Beloved"]
-        }
-      }
-    ];
+// Inyectar tarjeta en el container
+CARDS_CONTAINER.appendChild(card);
