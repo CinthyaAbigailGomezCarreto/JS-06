5776a051b52bc6a7ee52f36de1df8a3b15c11f2f
CinthyaAbigailGomezCarreto <164213827+CinthyaAbigailGomezCarreto@users.noreply.github.com>
2024-04-30 16:46:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-/// DOM API
-const div1 = document.getElementById("div-1");
-console.log(div1);
+// DOOM API
+// OBTENER EL POR ID
 
-const divColletion = document.getElementsByName("div")
-console.log(divColletion);
+const div1 = document.getElementById("div-1");
+console.log(div1); // No es coleccion porque solo tiene un elemento
+
+const divCollection = document.getElementsByTagName("div"); // lo busco por el nombre de la etiqueta div
+console.log(divCollection); // este sí es un arreglo porque tiene 2 elementos
 
 const p = document.getElementById("p1");
-const usernameInputs = document.getElementsByName("usename");
+const usernameInputs = document.getElementsByName("username");
 const usernameInput = usernameInputs[0];
-
 console.log(p, usernameInput);
 
+// tiene texto, nombre y valor
 usernameInput.addEventListener ("input", (event) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
     p.innerText = event.target.value;
-}
-)
+});
 
 
 
